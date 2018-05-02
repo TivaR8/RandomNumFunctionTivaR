0cea8a3f17448f7f1e5db0528c1d2b2eeeac034c
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-02 11:45:19 -0400

    Initial commit

---
+++ RandomNumFunctionTivaR/RandNumFunctionForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace RandomNumFunctionTivaR
+{
+    partial class frmRandNumFunction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmRandNumFunction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 289);
+            this.Name = "frmRandNumFunction";
+            this.Text = "Random Number Function by Tiva Rait";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ RandomNumFunctionTivaR/RandNumFunctionForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumFunctionTivaR
+{
+    public partial class frmRandNumFunction : Form
+    {
+        public frmRandNumFunction()
+        {
+            InitializeComponent();
+        }
+    }
+}
