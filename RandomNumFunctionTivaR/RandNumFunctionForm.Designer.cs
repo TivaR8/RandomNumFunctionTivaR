4608324539a7cecf6f3fa3c0ae3039502ad098a4
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-08 14:19:46 -0400

    Finished

--- RandomNumFunctionTivaR/RandNumFunctionForm.Designer.cs
+++ RandomNumFunctionTivaR/RandNumFunctionForm.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGenerateRandNum = new System.Windows.Forms.Button();
+            this.nudMaxValue = new System.Windows.Forms.NumericUpDown();
+            this.lblRandomNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxValue)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnGenerateRandNum
+            // 
+            this.btnGenerateRandNum.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnGenerateRandNum.Location = new System.Drawing.Point(98, 27);
+            this.btnGenerateRandNum.Name = "btnGenerateRandNum";
+            this.btnGenerateRandNum.Size = new System.Drawing.Size(127, 94);
+            this.btnGenerateRandNum.TabIndex = 0;
+            this.btnGenerateRandNum.Text = "Generate Random Number ";
+            this.btnGenerateRandNum.UseVisualStyleBackColor = true;
+            this.btnGenerateRandNum.Click += new System.EventHandler(this.btnGenerateRandNum_Click);
+            // 
+            // nudMaxValue
+            // 
+            this.nudMaxValue.Location = new System.Drawing.Point(99, 166);
+            this.nudMaxValue.Name = "nudMaxValue";
+            this.nudMaxValue.Size = new System.Drawing.Size(126, 20);
+            this.nudMaxValue.TabIndex = 1;
+            // 
+            // lblRandomNumber
+            // 
+            this.lblRandomNumber.AutoSize = true;
+            this.lblRandomNumber.Location = new System.Drawing.Point(98, 222);
+            this.lblRandomNumber.Name = "lblRandomNumber";
+            this.lblRandomNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblRandomNumber.TabIndex = 2;
+            this.lblRandomNumber.Text = "label1";
             // 
             // frmRandNumFunction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(367, 289);
+            this.Controls.Add(this.lblRandomNumber);
+            this.Controls.Add(this.nudMaxValue);
+            this.Controls.Add(this.btnGenerateRandNum);
             this.Name = "frmRandNumFunction";
             this.Text = "Random Number Function by Tiva Rait";
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxValue)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerateRandNum;
+        private System.Windows.Forms.NumericUpDown nudMaxValue;
+        private System.Windows.Forms.Label lblRandomNumber;
     }
 }
 
--- RandomNumFunctionTivaR/RandNumFunctionForm.cs
+++ RandomNumFunctionTivaR/RandNumFunctionForm.cs
@@ -7,6 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 08-05-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #30 - Percentage Program (Random Number Function)
+ * This program allows the user to set a parameter of the maximum number they...
+ * ...would like to receive and then the program gives the user a number in ...
+ * ... between 1 and the maximum value that the user selected.
+*/
 
 namespace RandomNumFunctionTivaR
 {
@@ -15,6 +24,40 @@ namespace RandomNumFunctionTivaR
         public frmRandNumFunction()
         {
             InitializeComponent();
+            lblRandomNumber.Hide();
+        }
+
+        // Function: GenerateRandomNumber
+        // Input: int minimum, int maximum
+        // Output: int
+        // Description: This function returns a random number between the given minimum and maximum values.
+        private int GenerateRandomNumber(int minimum, int maximum)
+        {
+            Random randomNumberGenerator = new Random();
+            int aRandomNumber;
+
+            aRandomNumber = randomNumberGenerator.Next(minimum, maximum + 1);
+
+            return aRandomNumber;
+        }
+
+        private void btnGenerateRandNum_Click(object sender, EventArgs e)
+        {
+            // Declare local variables
+            int userRandomNumber;
+            int userMaxValue;
+
+            // Get the max value from the user 
+            userMaxValue = Convert.ToInt16(this.nudMaxValue.Value);
+
+            // Call the function to return a random number
+            userRandomNumber = GenerateRandomNumber(1, userMaxValue);
+
+            // Show the label
+            lblRandomNumber.Show();
+
+            // Display the random number on the label
+            this.lblRandomNumber.Text = "The random number is: " + userRandomNumber;
         }
     }
 }
